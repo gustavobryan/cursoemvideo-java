85842c7147823d2a333beb82ba6364e43fce1d3b
Gustavo Bryan <devgustavob@gmail.com>
2023-05-21 22:06:48 -0300

    remove coment

--- src/main/java/cursoemvideo/olamundo/OlaMundoJava.java
+++ src/main/java/cursoemvideo/olamundo/OlaMundoJava.java
@@ -77,7 +77,7 @@ public class OlaMundoJava extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        
         jLabel1.setText("Oi, sou o Gustavo");
     }//GEN-LAST:event_jButton1ActionPerformed
 
